3cbe702a35b30a97fdb6bc633988a4763a4bb18d
wfay42 <wfay42@gmail.com>
2023-03-22 00:15:36 -0400

    Removed original raw_img/convert.bat

--- raw_img/convert.bat
+++
@@ -1,2 +0,0 @@
-magick convert -crop +700+0+1000x1440 -crop -860+0+1000x1440 hazel-01.png _c-hazel-01.png
-magick convert -crop +700+0+1000x1440 -crop -860+0+1000x1440 hazel-naga-03.png _c-hazel-naga-03.png
